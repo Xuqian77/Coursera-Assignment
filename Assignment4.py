17544915813976d8a216db33752ccc3de2b2874d
Xuqian77 <54742853+Xuqian77@users.noreply.github.com>
2022-10-30 09:21:38 +0800

    Add files via upload

---
+++ Assignment4.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('GDP and Electricity Cnsumption in Suzhou.csv')
+print(df)
+
+plt.figure()
+plt.plot(df['year'], df['GDP/(billion yuan)'],
+         df['year'], df['electricity consumption/(hundred million kwh)'],
+         df['year'], df['Energy Consumption per Unit of GDP / ( hundred million kwh/trillion yuan)'], linewidth=2)
+plt.legend(loc = "best", fontsize = 8,
+           labels=['GDP(billion yuan)', 'Electricity Consumption(hundred million kwh)',
+                   "Energy Consumption per Unit of GDP(hundred million kwh/trillion yuan)"])
+
+plt.xlabel('Years', alpha=0.8)
+plt.ylabel("GDP or Electricity Consumption", alpha = 0.8)
+plt.title("GDP and Electricity Consumption in Suzhou", alpha = 0.8)
+
+plt.gca().spines['top'].set_visible(False)
+plt.gca().spines['right'].set_visible(False)
+plt.show()
